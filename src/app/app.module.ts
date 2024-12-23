7d28eefc52e75eead60aab52b9787fc2d0e56c0c
tayfun.keles tayfun.keles <tayfun.keles@aurorabilisim.com>
2024-12-23 10:42:25 +0300

    Material versiyon ile angular versiyon arasındaki uym sorunu giderildi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
-import { FormsModule } from '@angular/forms';
+import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+import {MaterialExampleModule} from '../material.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    ToDoListComponent
+
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
+    MaterialExampleModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
