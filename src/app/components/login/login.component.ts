915b734ca6bfdaa72b247b57dd5ce67a79e3549d
tayfun.keles tayfun.keles <tayfun.keles@aurorabilisim.com>
2025-02-02 20:57:26 +0300

    Login sayfası yapıldı.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -11,12 +11,23 @@ export class LoginComponent implements OnInit {
 
   constructor(private fb:FormBuilder) {
     this.loginForm = this.fb.group({
-      email:['',Validators.required],
+      email:['',[Validators.required,Validators.email]],
       password:['',Validators.required]
    });
+   
   }
 
+
   ngOnInit(): void {
+    console.log("email",this.loginForm.get('email')?.value);
+    this.emailChanges();
+    
+  }
+  emailChanges(){
+  this.loginForm.get('email')?.valueChanges.subscribe((value)=>{
+    console.log("email",value);
+    
+  })
   }
 
 }
